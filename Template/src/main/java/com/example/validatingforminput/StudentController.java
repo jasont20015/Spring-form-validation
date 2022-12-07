8562fc9481116681ec6370f86e9c6d039e3310ef
Folix0 <theshadowcamel@gmail.com>
2022-12-08 00:36:39 +0100

    bugfixed files

--- Template/src/main/java/com/example/validatingforminput/StudentController.java
+++ Template/src/main/java/com/example/validatingforminput/StudentController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 public class StudentController implements WebMvcConfigurer {
     //TODO: Add spring annotations + Spring mapping annotations
 
@@ -16,15 +15,14 @@ public class StudentController implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/results").setViewName("results");
     }
-
     public String showForm(Student student) {
-        return "student";
+        return "form";
     }
 
     public String checkStudentInfo(Student studentForm, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            return "student";
+            return "form";
         }
 
         return "redirect:/results";
